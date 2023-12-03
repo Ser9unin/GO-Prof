6a55441ac04690a87d1112ab671fe0678f487c91
unknown <rockerror@gmail.com>
2023-12-03 18:58:39 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
 	// Place your code here.
+	var greeting = "Hello, OTUS!"
+
+	fmt.Println(reverse.String(greeting))
 }
