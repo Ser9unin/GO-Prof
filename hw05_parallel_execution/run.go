2e8db84d723111d6019c5e4f866295f1e602c5d7
unknown <rockerror@gmail.com>
2024-01-28 17:40:47 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,62 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var (
+		wg     sync.WaitGroup
+		mCheck int
+		err    error
+	)
+
+	tasksChan := make(chan Task)
+	errChan := make(chan error)
+	doneChan := make(chan struct{})
+
+	wg.Add(1)
+	go func() {
+		defer close(tasksChan)
+		for _, task := range tasks {
+			select {
+			case tasksChan <- task:
+				fmt.Println("write to taskChan")
+			case <-doneChan:
+				return
+			}
+		}
+		wg.Done()
+	}()
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(i int) {
+			for task := range tasksChan {
+				fmt.Println("worker", i, "read from taskChan")
+				err := task()
+				select {
+				case errChan <- err:
+					fmt.Println("write ", err)
+				case <-doneChan:
+					return
+				}
+			}
+			wg.Done()
+		}(i)
+	}
+
+	for j := 0; j < len(tasks); j++ {
+		t, ok := <-errChan
+		if !ok {
+			break
+		}
+		if t != nil {
+			mCheck++
+		}
+		if mCheck >= m {
+			err = ErrErrorsLimitExceeded
+			close(doneChan)
+			return err
+		}
+	}
+
+	wg.Wait()
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,33 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("no tasks does nothing", func(t *testing.T) {
+		tasks := make([]Task, 0)
+
+		result := Run(tasks, 10, 10)
+		require.Nil(t, result)
+	})
+
+	t.Run("concurency test with require.eventually", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Eventually(t, func() bool { return err != nil },
+			time.Second, time.Millisecond, "tasks were run sequentially?")
+	})
 }
