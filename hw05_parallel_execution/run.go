51e5b28288b01d9cc9daa8878076ae0642dc8e83
unknown <rockerror@gmail.com>
2024-01-29 22:34:38 +0300

    HW5 is completed fix 9

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,8 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
-	"runtime"
 	"sync"
 )
 
@@ -13,7 +11,6 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-
 	var (
 		wg     sync.WaitGroup
 		mCheck int
@@ -22,15 +19,12 @@ func Run(tasks []Task, n, m int) error {
 
 	tasksChan := make(chan Task)
 	errChan := make(chan error)
-
 	doneChan := make(chan struct{})
 
 	for i := 0; i < n; i++ {
-		i := i
 		wg.Add(1)
 		go func(tasksChan <-chan Task, errChan chan<- error, doneChan <-chan struct{}) {
 			defer wg.Done()
-			fmt.Println("Worker started", i)
 			for {
 				task, ok := <-tasksChan
 				if !ok {
@@ -39,9 +33,7 @@ func Run(tasks []Task, n, m int) error {
 				err := task()
 				select {
 				case errChan <- err:
-					fmt.Println("read ", err)
 				case <-doneChan:
-					fmt.Println("Worker stopped", i)
 					return
 				}
 			}
@@ -52,11 +44,9 @@ func Run(tasks []Task, n, m int) error {
 	go func(tasksChan chan<- Task, tasks []Task, doneChan <-chan struct{}) {
 		defer wg.Done()
 		defer close(tasksChan)
-		// defer close(errChan)
-		for i, task := range tasks {
+		for _, task := range tasks {
 			select {
 			case tasksChan <- task:
-				fmt.Println("write to taskChan", i)
 			case <-doneChan:
 				return
 			}
@@ -64,7 +54,6 @@ func Run(tasks []Task, n, m int) error {
 	}(tasksChan, tasks, doneChan)
 
 	for j := 0; j < len(tasks); j++ {
-
 		t, ok := <-errChan
 		if !ok {
 			break
@@ -73,9 +62,6 @@ func Run(tasks []Task, n, m int) error {
 			mCheck++
 			if mCheck >= m {
 				err = ErrErrorsLimitExceeded
-				fmt.Println("Goroutines running", runtime.NumGoroutine())
-				fmt.Println("Done channel closed")
-
 				break
 			}
 		}
@@ -83,6 +69,6 @@ func Run(tasks []Task, n, m int) error {
 
 	close(doneChan)
 	wg.Wait()
-	fmt.Println("Goroutines running", runtime.NumGoroutine())
+
 	return err
 }
