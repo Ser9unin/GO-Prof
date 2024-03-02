2763958a2628b7816a5c694738f51fc4afdd44ca
unknown <rockerror@gmail.com>
2024-03-02 12:35:33 +0300

    hw07 done

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,211 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCopy(t *testing.T) {
+
+	// Place your code here.
+
+	var err error
+
+	t.Run("copy complete file", func(t *testing.T) {
+
+		err = Copy("testdata/input.txt", "testdata/output.txt", 0, 0)
+
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+
+		require.NoError(t, err)
+
+		expected, err := os.Open("testdata/input.txt")
+
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		expectedByte, _ := io.ReadAll(expected)
+
+		require.Equal(t, expectedByte, actualByte)
+
+		expected.Close()
+
+		actual.Close()
+
+		_ = os.Remove("testdata/output.txt")
+
+	})
+
+	t.Run("copy limit 100", func(t *testing.T) {
+
+		err := Copy("testdata/input.txt", "testdata/output.txt", 0, 100)
+
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, 100, len(actualByte))
+
+		actual.Close()
+
+		_ = os.Remove("testdata/output.txt")
+
+	})
+
+	t.Run("copy negative offset", func(t *testing.T) {
+
+		err := Copy("testdata/input.txt", "testdata/output.txt", -10, 0)
+
+		require.Error(t, err)
+
+	})
+
+	t.Run("copy limit bigger than file", func(t *testing.T) {
+
+		err := Copy("testdata/input.txt", "testdata/output.txt", 0, 9999999)
+
+		require.NoError(t, err)
+
+		srcFile, _ := os.Open("testdata/input.txt")
+
+		srcFileStat, _ := srcFile.Stat()
+
+		srcFileSize := srcFileStat.Size()
+
+		actual, err := os.Open("testdata/output.txt")
+
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, int(srcFileSize), len(actualByte))
+
+		srcFile.Close()
+
+		actual.Close()
+
+		_ = os.Remove("testdata/output.txt")
+
+	})
+
+	t.Run("copy offset 100 limit 100", func(t *testing.T) {
+
+		err := Copy("testdata/input.txt", "testdata/output.txt", 100, 100)
+
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, 100, len(actualByte))
+
+		actual.Close()
+
+		_ = os.Remove("testdata/output.txt")
+
+	})
+
+}
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	var err error
+
+	t.Run("copy complete file", func(t *testing.T) {
+		err = Copy("testdata/input.txt", "testdata/output.txt", 0, 0)
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+		require.NoError(t, err)
+
+		expected, err := os.Open("testdata/input.txt")
+
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+		expectedByte, _ := io.ReadAll(expected)
+
+		require.Equal(t, expectedByte, actualByte)
+
+		expected.Close()
+		actual.Close()
+		_ = os.Remove("testdata/output.txt")
+	})
+
+	t.Run("copy limit 100", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/output.txt", 0, 100)
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, 100, len(actualByte))
+
+		actual.Close()
+		_ = os.Remove("testdata/output.txt")
+	})
+
+	t.Run("copy negative offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/output.txt", -10, 0)
+
+		require.Error(t, err)
+	})
+
+	t.Run("copy limit bigger than file", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/output.txt", 0, 9999999)
+		require.NoError(t, err)
+
+		srcFile, _ := os.Open("testdata/input.txt")
+		srcFileStat, _ := srcFile.Stat()
+		srcFileSize := srcFileStat.Size()
+
+		actual, err := os.Open("testdata/output.txt")
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, int(srcFileSize), len(actualByte))
+
+		srcFile.Close()
+		actual.Close()
+		_ = os.Remove("testdata/output.txt")
+	})
+
+	t.Run("copy offset 100 limit 100", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/output.txt", 100, 100)
+		require.NoError(t, err)
+
+		actual, err := os.Open("testdata/output.txt")
+		require.NoError(t, err)
+
+		actualByte, _ := io.ReadAll(actual)
+
+		require.Equal(t, 100, len(actualByte))
+
+		actual.Close()
+		_ = os.Remove("testdata/output.txt")
+	})
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,46 @@ package main
 
 import (
 	"flag"
+	"log"
+)
+
+var (
+	from, to string
+
+	limit, offset int64
+)
+
+func init() {
+
+	flag.StringVar(&from, "from", "", "file to read from")
+
+	flag.StringVar(&to, "to", "", "file to write to")
+
+	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
+
+	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+
+}
+
+func main() {
+
+	flag.Parse()
+
+	// Place your code here.
+
+	err := Copy(from, to, limit, offset)
+
+	if err != nil {
+
+		log.Println(err)
+
+	}
+
+}
+
+import (
+	"flag"
+	"log"
 )
 
 var (
@@ -18,5 +58,10 @@ func init() {
 
 func main() {
 	flag.Parse()
+
 	// Place your code here.
+	err := Copy(from, to, limit, offset)
+	if err != nil {
+		log.Println(err)
+	}
 }
