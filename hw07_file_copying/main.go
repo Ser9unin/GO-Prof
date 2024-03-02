08e448b985da9ac63f8ae58ac43ec1ad02f25ed3
unknown <rockerror@gmail.com>
2024-03-02 12:36:52 +0300

    hw07 done

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -6,45 +6,6 @@ import (
 )
 
 var (
-	from, to string
-
-	limit, offset int64
-)
-
-func init() {
-
-	flag.StringVar(&from, "from", "", "file to read from")
-
-	flag.StringVar(&to, "to", "", "file to write to")
-
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
-
-}
-
-func main() {
-
-	flag.Parse()
-
-	// Place your code here.
-
-	err := Copy(from, to, limit, offset)
-
-	if err != nil {
-
-		log.Println(err)
-
-	}
-
-}
-
-import (
-	"flag"
-	"log"
-)
-
-var (
 	from, to      string
 	limit, offset int64
 )
