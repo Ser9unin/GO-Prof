be7388050a013ce73a0b213e7b7aa437a46efb31
unknown <rockerror@gmail.com>
2024-03-04 18:32:11 +0300

    hw08 start

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -12,5 +14,9 @@ type EnvValue struct {
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
+	if files, err := os.ReadDir(dir); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
